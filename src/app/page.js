091554ee3e192c9cf4131ff647327521dfc72a4a
SkyeFull90 <122103934+SkyeFull90@users.noreply.github.com>
2023-08-01 19:59:53 -0600

    updated  project in this repo

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,72 @@
-import Image from 'next/image'
+import { Indie_Flower } from 'next/font/google'
 
+const indie = Indie_Flower({weight: '400', subsets: ['latin']})
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <div className={'bg-cover bg-center bg-no-repeat'} style={{backgroundImage: 'url(/cottage-core-trend-alert-1.png)'}}>
+      <header>
+          <section className="intro gap-2">
+              <h1 className="about-me text-5xl font-bold">Skyler Fullmer</h1>
+              <div className={'breadcrumbs'}>
+                  <ul>
+                      <li><a href="#climbing" className="climber">Climber </a></li>
+                      <li><a href="#web-dev" className="developer">Developer </a></li>
+                      <li><a href="#youtube" className="producer">Video Producer </a></li>
+                  </ul>
+              </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+              <div className={`${indie.className}`}>
+                  <p className="about-me-p">Passionate about Rock Climbing, software development, gaming, Cosplay, and Public land access for those in the Rock Climbing Space.</p>
+                  <p className="about-me-p">Inspired by the mountains and cliffs I tend to explore. As well as gaming, film, tv, and anime. </p>
+              </div>
+            <img src={'/IMG_20211105_223720_412.jpg'} height={250} width={250}/>
+          </section>
+      </header>
+      <main>
+          <section className="portfolio-web" id="web-dev">
+              <h1>view my portfolio</h1>
+              <p>click to view all the websites, and personal projects</p>
+              <button className={'btn btn-outline btn-glass'}>
+                  <a href="/webportfolio">view portfolio</a>
+              </button>
+          </section>
+          <section className={'rock-climbing'} id={'climbing'}>
+              <h1>My Tick List can be viewed: <a href="https://www.mountainproject.com/user/112227120/skyler-fullmer/ticks" id="climber">Here</a></h1>
+              <h3>And below are a few routes I have really enjoyed.</h3>
+              <section className={'tick-list'}>
+                  <section>
+                      <div className="carousel carousel-center rounded-box">
+                          <div className="carousel-item">
+                              <img src="/desertRats.jpg" height={250} width={250}  />
+                          </div>
+                          <div className="carousel-item ">
+                              <img src="/teamTrailer.jpg" height={250} width={250}  />
+                          </div>
+                          <div className="carousel-item ">
+                              <img src="/110895792_large_1494357679.jpg" height={250} width={250}  />
+                          </div>
+                      </div>
+                  </section>
+                 <section>
+                     <h2><a href="https://www.mountainproject.com/route/108178999/desert-rats-be-damned"> Desert Rats Be Damned</a></h2>
+                     <h2><a href="https://www.mountainproject.com/route/107695410/team-trailer">Team Trailer</a></h2>
+                     <h2><a href="https://www.mountainproject.com/route/105841378/annie-git-yer-drill">Annie Git Yer Drill </a></h2>
+                 </section>
+              </section>
+              <h3>And click the button to view more info about my favorite routes</h3>
+              <button className={'btn glass btn-outline'}><a href="/photostash">The Stash</a></button>
+          </section>
+          <section className="portfolio-video" id="youtube">
+              <h1>view the videos I have produced. some only with a samsung a51 5g</h1>
+              <h3>The Climb Vlog: A vlog about climbing, Life, and the Climbing Life</h3>
+              <p>click to view my YouTube</p>
+              <button>
+                  <a href="https://www.youtube.com/@QuirkyGek/featured">Watch The Climb Vlog</a>
+              </button>
+              <span>I also Stream on twitch: No Man's Sky, Halo Infinite, Halo the Masterchief Collection, and Warframe</span>
+              <button><a href="https://twitch.tv/thequirkygek">Join in on the space chaos</a></button>
+          </section>
+      </main>
+    </div>
   )
 }
