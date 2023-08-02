224aa2ae6f8abe9519498770178f6c2e4e0b9fec
SkyeFull90 <122103934+SkyeFull90@users.noreply.github.com>
2023-08-02 16:19:28 -0600

    buildong more pages

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Profile(){
     return(
-        <div>
+        <div className="pt-24 text-2xl">
             <h1>A Few Things To Know...</h1>
         </div>
     )
--- src/app/webportfolio/page.js
+++ src/app/webportfolio/page.js
@@ -2,6 +2,17 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div>page</div>
+    <div>
+      <div className="hero min-h-screen" style={{backgroundImage: 'url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)'}}>
+          <div className="hero-overlay bg-opacity-60"></div>
+            <div className="hero-content text-center text-neutral-content">
+              <div className="max-w-md">
+                <h1 className='text-5xl font-bold'>Hello Friend</h1>
+                <p className='py-6'>Welcome to the Code Stash. Her you will find alot of my Projects. Click below to view more of what I have to offer.</p>
+                <button className="btn btn-primary">view the GitHub for more projects</button>
+              </div>
+          </div>
+        </div>
+    </div>
   )
 }
