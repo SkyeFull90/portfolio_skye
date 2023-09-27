d43cffbb772e39a88aa751fa56d4994c6d54534f
Skye Fulmer <122103934+SkyeFull90@users.noreply.github.com>
2023-09-27 14:08:24 -0600

    Updated web portfolio page links and footer links

--- src/app/webportfolio/page.js
+++ src/app/webportfolio/page.js
@@ -20,7 +20,7 @@ export default function page() {
                 <div className={'gap-4'}>
                     <h1 className="text-5xl font-bold mb-4">Events by 3</h1>
                     <p className="mb-4">Currently under construction Events by 3. My first Full-Stack site. This one is for world events and sharing them with y"all out there. It is being rebuilt with Mongoose and a brand new dataset. With more responsive features to work and a better UI. It is built on NextJS a react framework ready for production with a built in dev server. Read the latest docs on NextJS: <a href={'https://nextjs.org/'}>Here</a></p>
-                    <a href={'https://world-events.netlify.app/'}><button className={'btn btn-primary mr-3'}>View the site here</button></a>
+                    <a href={'#'}><button className={'btn btn-disabled mr-3'}>View the site here</button></a>
                     <a href={'https://github.com/SkyeFull90/world-events'}><button className={'btn btn-ghost'}>And the Source code here</button></a>
                 </div>
             </div>
@@ -31,7 +31,7 @@ export default function page() {
                 <div>
                     <h1 className="text-5xl font-bold">Nextra Portfolio and Blog</h1>
                     <p className="py-6">This one is my first attempt at a portfolio site built with Nextra a framework with NextJS and Markdown uses MDX a markdown and jsx mix to build pages and content. Read more about Nextra: <a href={'https://nextra.site/'}>Here</a></p>
-                    <a href={'https://skyeportflio.netlify.app/'}><button className="btn btn-primary mr-3">Get Started</button></a>
+                    <a href={'https://portfolio-phi-black.vercel.app/'}><button className="btn btn-primary mr-3">Get Started</button></a>
                     <a href={'https://github.com/SkyeFull90/portfolio'}><button className={'btn btn-ghost'}>View the Source Code</button></a>
                 </div>
             </div>
@@ -41,7 +41,7 @@ export default function page() {
                 <img src="/pythonSalesDataSorter.png" className="max-w-sm rounded-lg shadow-2xl"  alt={'python'}/>
                 <div>
                     <h1 className="text-5xl font-bold">Python Sales Data Sorter</h1>
-                    <p className="py-6">A sales data sorter console app originally built in java and rebuilt in python and javascript for better readability and code efficiency. Soon py test will be add for accuracy and coverage, as well as jest for js vers.</p>
+                    <p className="py-6">A sales data sorter console app originally built in java and rebuilt in python and javascript for better readability and code efficiency. Soon py test will be added for accuracy and coverage, as well as jest for js vers. Currently working on a Django version that sends a report to a SQLLite database then reads from it. To display the report on a dashboard.</p>
                     <a href={'https://github.com/SkyeFull90/salesDataSorter'}><button className="btn btn-primary mr-3">View the source code and have fun with this app</button></a>
                 </div>
             </div>
